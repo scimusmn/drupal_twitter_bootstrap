39b56e04c77df3da9e988f14b775ca402cdd4ebc
Sarah German <sgerman@smm.org>
2012-02-27 12:48:15 -0600

    Pulled in logo variable to site header, adjusted primary tab position accordingly

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -34,7 +34,7 @@
       <div class="span5">
         <?php if ($logo): ?>
           <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-          <img src="<?php print base_path().path_to_theme(). '/images/logo.png'; ?>" alt="Mystery Experience" />
+          <img src="<?php print $logo; ?>" alt="<?php print $site_name; ?>" />
           </a>
         <?php endif; ?>
       </div>
